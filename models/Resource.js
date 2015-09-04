cd371f1833756ab161ca438430dc0f22f75e9515
hews <p@hews.co>
2015-09-03 23:30:53 -0700

    Update the model to make the test pass!

--- models/Resource.js
+++ models/Resource.js
@@ -1,7 +1,18 @@
 var mongoose = require('mongoose');
 
-var ResourceSchema = new mongoose.Schema({
+var resourceSchema = new mongoose.Schema({
   name: String
 });
 
-module.exports = mongoose.model('Resource', ResourceSchema);
+var jsonFormatter = function(document, returnedObject, options) {
+  returnedObject.id = returnedObject._id
+  delete returnedObject._id;
+  delete returnedObject.__v;
+  return returnedObject;
+};
+
+resourceSchema.options.toJSON = {
+  transform: jsonFormatter
+};
+
+module.exports = mongoose.model('Resource', resourceSchema);
